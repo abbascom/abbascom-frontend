66cb9660fad1fd0b17f9aa0d6917cdf014f455e0
abbascom <accounts@xeriyab.com>
2025-07-03 13:32:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
-// Main App component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import InventoryList from "./components/Inventory/InventoryList";
+import ExpenseList from "./components/Expense/ExpenseList";
+import SalesList from "./components/Sales/SalesList";
+import Login from "./components/Auth/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/inventory" element={<InventoryList />} />
+        <Route path="/expense" element={<ExpenseList />} />
+        <Route path="/sales" element={<SalesList />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
