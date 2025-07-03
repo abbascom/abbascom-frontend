d19b8634a6a25027aa989de1ab802a12554b30aa
abbascom <accounts@xeriyab.com>
2025-07-03 13:45:29 +0530

    Update ExpenseList.jsx

--- src/components/Expense/ExpenseList.jsx
+++ src/components/Expense/ExpenseList.jsx
@@ -1,1 +1,12 @@
-// Expense list component placeholder
+import React from "react";
+
+const ExpenseList = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Expense List</h2>
+      <p>Display expense entries here.</p>
+    </div>
+  );
+};
+
+export default ExpenseList;
